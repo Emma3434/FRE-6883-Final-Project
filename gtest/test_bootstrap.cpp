3471ad5256554309e78d12e6ba158a4002941bdd
ken.chen <chken4869@hotmail.com>
2021-05-06 00:27:18 +0800

    test: minor change

--- gtest/test_bootstrap.cpp
+++ gtest/test_bootstrap.cpp
@@ -20,11 +20,11 @@ protected:
 };
 
 TEST_F(BootstrapTest, resultSize) {
-    vector<StockData> stock_list_miss = load_stock_data(MISS_FILE, "Miss");
-    vector<StockData> stock_list_meet = load_stock_data(MEET_FILE, "Meet");
-    vector<StockData> stock_list_beat = load_stock_data(BEAT_FILE, "Beat");
-    vector<StockData> stock_list = combine_stock_list(stock_list_miss, stock_list_meet, stock_list_beat);
-    map<string, vector<StockData>> bootstrap_result;
+    vector<StockData*> stock_list_miss = load_stock_data(MISS_FILE, "Miss");
+    vector<StockData*> stock_list_meet = load_stock_data(MEET_FILE, "Meet");
+    vector<StockData*> stock_list_beat = load_stock_data(BEAT_FILE, "Beat");
+    vector<StockData*> stock_list = combine_stock_list(stock_list_miss, stock_list_meet, stock_list_beat);
+    map<string, vector<StockData*>> bootstrap_result;
     bootstrap_result = bootstrapping(stock_list);
 
     EXPECT_EQ(50, bootstrap_result["Beat"].size());
