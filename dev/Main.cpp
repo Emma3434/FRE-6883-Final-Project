2d1bf7c11783a4ede6e98e9617b20755d66f3028
Jingzhao Zhang <zhangjingzhao32@163.com>
2021-05-05 15:50:09 +0800

    feat: add test for Matrix

--- dev/Main.cpp
+++ dev/Main.cpp
@@ -9,6 +9,7 @@
 #include <time.h>
 #include <thread>
 //#include <map>
+#include "Matrix.h"
 
 #include <iostream>
 using namespace std;
@@ -20,6 +21,7 @@ int main()
 {
 
 	vector<StockData*> stock_list = load_stock_data(TEST_FILE, "Test");
+
 
 
 
@@ -41,15 +43,15 @@ int main()
 	// - finish
 	/*cout << "--------------------------------------------" << endl;
 	cout << "start test load_stock_dta..." << endl << endl;
-	vector<StockData*> stock_list_miss = load_stock_data(MISS_FILE, "Miss");
-	vector<StockData*> stock_list_meet = load_stock_data(MEET_FILE, "Meet");
-	vector<StockData*> stock_list_beat = load_stock_data(BEAT_FILE, "Beat");
+	vector<StockData> stock_list_miss = load_stock_data(MISS_FILE, "Miss");
+	vector<StockData> stock_list_meet = load_stock_data(MEET_FILE, "Meet");
+	vector<StockData> stock_list_beat = load_stock_data(BEAT_FILE, "Beat");
 
-	cout << "stock list Miss size: " << stock_list_miss.size() << endl;
-	cout << "stock list Meet size: " << stock_list_meet.size() << endl;
-	cout << "stock list Beat size: " << stock_list_beat.size() << endl;
+	cout << "stock list Miss size£º " << stock_list_miss.size() << endl;
+	cout << "stock list Meet size£º " << stock_list_meet.size() << endl;
+	cout << "stock list Beat size£º " << stock_list_beat.size() << endl;
 
-	vector<StockData*> stock_list = combine_stock_list(stock_list_miss, stock_list_meet, stock_list_beat);
+	vector<StockData> stock_list = combine_stock_list(stock_list_miss, stock_list_meet, stock_list_beat);
 	
 	cout << "stock list size£º " << stock_list.size() << endl;*/
 
@@ -66,7 +68,7 @@ int main()
 	cout << "--------------------------------------------" << endl;
 	cout << "start test fetch data..." << endl << endl;
 
-	int N = 50;
+	int N = 2;
 
 	clock_t time1 = clock();
 	for (auto iter : stock_list)
@@ -76,16 +78,11 @@ int main()
 		iter->CalDailyReturns();
 		iter->DisplayData();
 	}
+		
 	clock_t time2 = clock();
 	double secs = (double)(time2 - time1) / CLOCKS_PER_SEC;	
 	cout << "run time : " << secs  << " seconds" << endl;
 	
-	for (auto iter : stock_list)
-	{
-		cout << iter->ticker << endl;
-		cout << iter->abnormal_return << endl;
-	}
-
 
 
 
@@ -138,19 +135,14 @@ int main()
 	//cout << "run time : " << secs << " seconds" << endl;
 
 
-
-
-
-
-
 	// test create stock map
 	// - finish
 
-	cout << "--------------------------------------------" << endl;
+	/*cout << "--------------------------------------------" << endl;
 	cout << "start test stock map..." << endl << endl;
-	map<string, StockData*> stock_map = create_stock_map(stock_list);
-	cout << "stock map size: " << stock_map.size() << endl;
-	stock_map["PWR"]->DisplayAttribute();
+	map<string, StockData> stock_map = create_stock_map(stock_list);
+	cout << "stock mat size: " << stock_map.size() << endl;
+	stock_map["PWR"].DisplayAttribute();*/
 
 
 
@@ -158,7 +150,7 @@ int main()
 	// test bootstrapping
 	// - 
 
-	/*map<string, vector<StockData*>> bootstrap_result;
+	/*map<string, vector<StockData>> bootstrap_result;
 	for (int i = 0; i < 3; i++)
 	{
 		cout << "bootstrapping trial: " << i << endl;
@@ -166,9 +158,9 @@ int main()
 		cout << "Beat length: " << bootstrap_result["Beat"].size() << endl;
 		cout << "Meet length: " << bootstrap_result["Meet"].size() << endl;
 		cout << "Miss length: " << bootstrap_result["Miss"].size() << endl;
-		bootstrap_result["Beat"][0]->DisplayAttribute();
-		bootstrap_result["Meet"][1]->DisplayAttribute();
-		bootstrap_result["Miss"][2]->DisplayAttribute();
+		bootstrap_result["Beat"][0].DisplayAttribute();
+		bootstrap_result["Meet"][1].DisplayAttribute();
+		bootstrap_result["Miss"][2].DisplayAttribute();
 		cout << endl;
 	}*/
 	
@@ -176,7 +168,7 @@ int main()
 	
 	//test vector class
 	//-
-	/*cout << "--------------------------------------------" << endl;
+	cout << "--------------------------------------------" << endl;
 	cout << "start test VectorClass..." << endl << endl;
 	vector<double> data = { 100,101,102,103,104 };
 	vector<double> data2 = { 99,100,101,102,103 };
@@ -184,51 +176,48 @@ int main()
 	Vector adjclose2(data2);
 	Vector adjclose3(data);
 	cout << "Original vector adjclose" << " ";
-	for (int i = 0; i < adjclose.size(); i++)
-	{
-		cout << adjclose[i] << " ";
-	}
-	cout << endl;
+	cout << adjclose << endl;
 
 	cout << "Test adjclose.size() " << adjclose.size() << endl;
 	adjclose.push_back(105);
-	cout << "After push_back(105) ";
-	for (int i = 0; i < adjclose.size(); i++)
-	{
-		cout << adjclose[i]<<" ";
-	}
-	cout << endl;
+	cout << "After push_back(105) " << adjclose << endl;
 	cout << "adjclose.back()" << adjclose.back() << endl;
 	Vector pctchange = adjclose.pct_change();
-	cout << "After pct_change() ";
-	for (int i = 0; i < pctchange.size(); i++)
-	{
-		cout << pctchange[i]<<" ";
-	}
-	cout << endl;
+	cout << "After pct_change() " << pctchange << endl;
 	cout << "Calculate sum of adjreturn " << pctchange.sum() << endl;
-	cout << "Calculate cumsum of adjreturn ";
-	for (int i = 0; i < pctchange.cumsum().size(); i++)
-	{
-		cout << pctchange.cumsum()[i] << " ";
-	}
-	cout << endl;
+	Vector cumsum = pctchange.cumsum();
+	cout << "Calculate cumsum of adjreturn " << cumsum << endl;
 	
 	Vector diff = adjclose3 - adjclose2;
 	Vector sum = adjclose3 + adjclose2;
+	cout << "Sum of data 3 and 2 " << sum << endl;
+	cout << "Difference of data 3 and 2 " << diff << endl;
 
-	cout << "Sum of data 3 and 2 ";
-	for (int i = 0; i < sum.size(); i++)
-	{
-		cout << sum[i] <<" ";
-	}
-	cout << endl;
-	cout << "Difference of data 3 and 2 ";
-	for (int i = 0; i < diff.size(); i++)
-	{
-		cout << diff.data[i] <<" ";
-	}
-	cout << endl;
+	//test matrix
+	cout << "--------------------------------------------" << endl;
+	cout << "start test Matrix..." << endl << endl;
+	
+	Matrix abnormal_return(stock_list);
+	
+		
+	cout << "Abnormal return matrix" <<endl<< abnormal_return << endl;
+	abnormal_return.append(abnormal_return.data.back());
+	cout << "Append another stock " << endl << abnormal_return << endl;
+	Vector Sum = abnormal_return.sum();
+	cout << " Sum by column" << endl << Sum << endl;
+	Vector mean = abnormal_return.mean();
+	cout << "Mean by column" << endl << mean << endl;
+	Vector std = abnormal_return.std();
+	cout << "Std by column" << endl << std << endl;
 
-	cout << "end" << endl;*/
+	for (auto itr : stock_list)
+	{
+		delete itr;
+		itr = NULL;
+	};
+		
+
+
+//
+	cout << "end" << endl;
 }
