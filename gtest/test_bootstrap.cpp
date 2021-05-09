3b5532c28f9f7e905cb36b9411d0a1eae9944562
ken.chen <chken4869@hotmail.com>
2021-05-10 00:23:25 +0800

    test: test matrix, change directory name

--- gtest/test_bootstrap.cpp
+++ gtest/test_bootstrap.cpp
@@ -12,20 +12,20 @@ protected:
     }
     virtual void SetUp() {
         //std::cout << "SetUp\n";
+        stock_list_miss = load_stock_data(MISS_FILE, "Miss");
+        stock_list_meet = load_stock_data(MEET_FILE, "Meet");
+        stock_list_beat = load_stock_data(BEAT_FILE, "Beat");
+        stock_list = combine_stock_list(stock_list_miss, stock_list_meet, stock_list_beat);
+        bootstrap_result = bootstrapping(stock_list, 1);
     }
     virtual void TearDown() {
         //std::cout << "TearDown\n";
     }
-    vector<StockData> stock_list;
+    vector<StockData*> stock_list_miss, stock_list_meet, stock_list_beat, stock_list;
+    map<string, vector<StockData*>> bootstrap_result;
 };
 
 TEST_F(BootstrapTest, resultSize) {
-    vector<StockData*> stock_list_miss = load_stock_data(MISS_FILE, "Miss");
-    vector<StockData*> stock_list_meet = load_stock_data(MEET_FILE, "Meet");
-    vector<StockData*> stock_list_beat = load_stock_data(BEAT_FILE, "Beat");
-    vector<StockData*> stock_list = combine_stock_list(stock_list_miss, stock_list_meet, stock_list_beat);
-    map<string, vector<StockData*>> bootstrap_result;
-    bootstrap_result = bootstrapping(stock_list);
 
     EXPECT_EQ(50, bootstrap_result["Beat"].size());
     EXPECT_EQ(50, bootstrap_result["Meet"].size());
